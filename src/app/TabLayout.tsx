460d8e173f13f75fd9a3bf878dd8a173fb5b362c
tam11a <ibrahimsadiktamim@gmail.com>
2023-12-14 19:13:22 +0600

    init app

---
+++ src/app/TabLayout.tsx
@@ -0,0 +1,34 @@
+import { Tabs, TabsProps } from "antd";
+import React from "react";
+
+const items: TabsProps["items"] = [
+	{
+		key: "1",
+		label: "Tab 1",
+		children: "Content of Tab Pane 1",
+	},
+	{
+		key: "2",
+		label: "Tab 2",
+		children: "Content of Tab Pane 2",
+	},
+	{
+		key: "3",
+		label: "Tab 3",
+		children: "Content of Tab Pane 3",
+	},
+];
+
+const TabLayout: React.FC = () => {
+	return (
+		<div>
+			<Tabs
+				defaultActiveKey="1"
+				size="small"
+				items={items}
+			/>
+		</div>
+	);
+};
+
+export default TabLayout;
---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from "react-dom/client";
+import TabLayout from "./TabLayout";
+
+const root = createRoot(document.body);
+root.render(
+	<>
+		<TabLayout />
+	</>
+);
